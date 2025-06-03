1fcb3a69ed37cfbb76b299b8cd59431a1773e85d
Gregory Bell <ghbell@gmail.com>
2025-06-03 07:05:02 -0500

    try to fix build

--- apps/organizers-app/vite.config.ts
+++ apps/organizers-app/vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["@rhythm-sync/database", "@rhythm-sync/shared-types"]
+  },
 }));
